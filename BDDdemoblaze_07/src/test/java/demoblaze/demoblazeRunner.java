a9615ffe5a94d1133e105efdbcf9b51d5869f42f
ShaikAsifa1 <108386275+ShaikAsifa1@users.noreply.github.com>
2022-07-08 09:11:44 +0530

    Update demoblazeRunner.java

--- BDDdemoblaze_07/src/test/java/demoblaze/demoblazeRunner.java
+++ BDDdemoblaze_07/src/test/java/demoblaze/demoblazeRunner.java
@@ -5,7 +5,16 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class) 
-@CucumberOptions(plugin = {"pretty", "html:target/Destination"},monochrome=true) 
+@CucumberOptions
+(plugin = {"pretty", "junit:target/report.xml",},
+tags="@ParameterizedTest",
+features="C:\\Users\\shivr\\eclipse-workspace\\BDDdemoblaze_07\\src\\test\\java\\demoblaze\\demoblaze.feature",
+glue="demoblaze",
+monochrome=true,
+dryRun=false
+)
+//"html:target/Destination",
+//"json:target/report.json"
 public class demoblazeRunner {
 
 }
